6b273c2e124d676eb612dfdb2a4afa02ad30e6e9
PiotrK <piotrek380@gmail.com>
2023-04-01 17:17:26 +0200

    feat(package.json): add react-icons dependency feat(Layout.tsx): add Sidebar component to Layout style(Layout.tsx): add container, grid, and border styles to Layout feat(Sidebar.tsx): create Sidebar component style(_app.tsx): remove semicolon from Component in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,21 @@
 import { ReactNode } from "react";
+import { Sidebar } from "./layout/Sidebar";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-  return <div className="bg-indigo-500">{children}</div>;
+  return (
+    <div className="bg-indigo-500">
+      <div className="container mx-auto px-5 border-2 lg:max-w-7xl">
+        <div className="grid grid-cols-4">
+          <Sidebar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1]">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
